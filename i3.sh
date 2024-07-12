3823ede869f7d818417477d69c686992a6b31dc7
to4o <didaka0099@abv.gb>
2024-07-12 20:58:32 +0300

    change i3

--- i3.sh
+++ i3.sh
@@ -1,3 +1,1 @@
-sudo apt install i3 dunst suckless-tools compton hsetroot rxvt-unicode xsel rofi fonts-noto fonts-mplus xsettingsd lxappearance scrot viewnior
-
-git clone https://github.com/addy-dclxvi/i3-starterpack.git && cp -a i3-starterpack/. ~
+sudo apt install i3 xorg lightdm lightdm-settings slick-greeter lxappearance nitrogen firefox-esr pulseaudio alsa-utils pavucontrol alacritty pcmanfm
