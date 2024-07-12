83935227b6790f592153e7c580a2b004ce267663
to4o <didaka0099@abv.gb>
2024-07-12 20:50:32 +0300

    add i3 and remove grup

--- essentials.sh
+++ essentials.sh
@@ -2,8 +2,3 @@
 
 # Must have apts 
 sudo apt install wget curl
-
-# Make grub wait 1 sec
-sed -i 's/GRUB_TIMEOUT=1/GRUB_TIMEOUT=0/g' /etc/default/grub 
-
-sudo update-grub
---
+++ i3.sh
@@ -0,0 +1,3 @@
+sudo apt install i3 dunst suckless-tools compton hsetroot rxvt-unicode xsel rofi fonts-noto fonts-mplus xsettingsd lxappearance scrot viewnior
+
+git clone https://github.com/addy-dclxvi/i3-starterpack.git && cp -a i3-starterpack/. ~
