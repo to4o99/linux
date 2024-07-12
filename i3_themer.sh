e78aff44ad826f0704fe8b157f200efda1630152
to4o <didaka0099@abv.gb>
2024-07-12 22:30:58 +0300

    add i3 themer

---
+++ i3_themer.sh
@@ -0,0 +1,20 @@
+sudo apt install \
+    dunst \
+    i3 \
+    i3lock-fancy \
+    polybar xwallpaper \
+    rofi \
+    bash \
+    fonts-font-awesome \
+    xdg-utils \
+    sensible-utils \
+    alsa-utils \
+    rxvt-unicode \
+    fonts-firacode \
+    flameshot \
+    xautolock \
+    numlockx
+
+git clone --depth=1 https://github.com/cizordj/i3-themer.git
+cd i3-themer/
+./i3-themer -a 013
