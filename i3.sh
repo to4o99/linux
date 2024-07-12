2b811070b89f587380efdc5dcce449f4a90313ad
to4o <didaka0099@abv.gb>
2024-07-12 21:41:16 +0300

    try i3 starter again

--- i3.sh
+++ i3.sh
@@ -1,1 +1,3 @@
-sudo apt install i3 xorg lightdm lightdm-settings slick-greeter lxappearance nitrogen firefox-esr pulseaudio alsa-utils pavucontrol alacritty pcmanfm
+sudo apt install xorg i3 dunst suckless-tools compton hsetroot rxvt-unicode xsel rofi fonts-noto fonts-mplus xsettingsd lxappearance scrot viewnior alacritty
+
+git clone https://github.com/addy-dclxvi/i3-starterpack.git
