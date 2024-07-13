e4062fcd6d909e9944578c517714aeae27b57611
to4o <didaka0099@abv.gb>
2024-07-13 23:10:57 +0300

    update i3

--- i3.sh
+++ i3.sh
@@ -1,4 +1,11 @@
-git clone https://github.com/DebianFanboy/debian-i3gaps-from-scratch.git
-cd debian-i3gaps-from-scratch
-chmod +x run.sh
-./run.sh
+sudo apt install xorg lightdm lightdm-settings i3 polybar dunst picom rofi
+
+git clone https://github.com/lasercata/Dotfiles.git
+
+sudo apt install zip unzip
+
+wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip \
+&& cd ~/.local/share/fonts \
+&& unzip JetBrainsMono.zip \
+&& rm JetBrainsMono.zip \
+&& fc-cache -fv
