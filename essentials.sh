0043e43d332f26b64d6b01110bf4773201b2fe2f
to4o <didaka0099@abv.gb>
2024-07-14 16:38:29 +0300

    up essentials

--- essentials.sh
+++ essentials.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
 
 # Must have apts 
-#sudo apt install wget curl zip unzip
+sudo apt install wget curl zip unzip
 
 # Git config
-#git config --global credential.helper store
-#git config --global alias.a '!git add -A && git commit -m'
+git config --global credential.helper store
+git config --global alias.a '!git add -A && git commit -m'
 
 # Append to bashrc
 cat ~/linux/append_to_bashrc.txt >> ~/.bashrc
