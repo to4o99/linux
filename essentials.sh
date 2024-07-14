b078559abb75c11781e2842e7b626660297f499e
to4o <didaka0099@abv.gb>
2024-07-14 16:10:55 +0300

    change essentials

--- essentials.sh
+++ essentials.sh
@@ -1,9 +1,13 @@
-#!/bin/bash
+#!/bin/bash 
 
 # Must have apts 
-sudo apt install wget curl
+#sudo apt install wget curl zip unzip
 
 # Git config
-git config --global credential.helper store
-git config --global alias.a '!git add -A && git commit -m'
+#git config --global credential.helper store
+#git config --global alias.a '!git add -A && git commit -m'
 
+# Append to bashrc
+cat ~/linux/append_to_bashrc.txt >> ~/.bashrc
+
+echo "Don't forget to source .bashrc"
